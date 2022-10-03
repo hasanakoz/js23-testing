ce7eb336868c273bb29f0cc7be1dd39d25c0f074
hasan <hasanakoz1986@gmail.com>
2022-10-03 22:43:32 +0300

    fibonacci function completely tested via npm test

--- src/math.test.js
+++ src/math.test.js
@@ -25,4 +25,24 @@ describe("celsiusToFahrenheit", () => {});
 
 describe("fahrenheitToCelsius", () => {});
 
-describe("fibonacci", () => {});
+describe("fibonacci", () => {
+  it("should exist", () => {
+    expect(fibonacci).toBeDefined();
+  });
+  it("should return a number", () => {
+    expect(fibonacci(1)).toBe(1);
+  });
+
+  it("should return 55 for 10th index", () => {
+    expect(fibonacci(10)).toBe(55);
+  });
+  it("should throw error if less than 1", () => {
+    expect(() => fibonacci(0)).toThrow(
+      new Error("Must enter a number greater than 0")
+    );
+  });
+
+  it("should return 1 for none", () => {
+    expect(fibonacci()).toBe(1);
+  });
+});
